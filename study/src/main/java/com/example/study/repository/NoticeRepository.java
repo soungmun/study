9d9424e8dc4c66feeeeb58089f5d4fa9643e73d0
smjo15 <smjo6343#gmail.com>
2026-05-05 09:56:57 +0900

    dasdds

--- study/src/main/java/com/example/study/repository/NoticeRepository.java
+++ study/src/main/java/com/example/study/repository/NoticeRepository.java
@@ -3,5 +3,5 @@ package com.example.study.repository;
 import com.example.study.entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+public interface NoticdqwqdwqdeRepository extends JpaRepository<Notice, Long> {
 }
